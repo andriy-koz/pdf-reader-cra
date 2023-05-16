f055e7b9bf334dfae57970518ffc119794a505a2
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-05-16 20:53:31 -0300

    styling

--- src/App.js
+++ src/App.js
@@ -12,7 +12,33 @@ function App() {
 
   return (
     <div className='app'>
-      <FileUploader onFileChange={handleFileChange} />
+      <div className='video-background'>
+        <video
+          autoPlay
+          loop
+          muted
+          style={{
+            position: 'absolute',
+            width: '100%',
+            left: '50%',
+            top: '50%',
+            height: '100%',
+            objectFit: 'cover',
+            transform: 'translate(-50%, -50%)',
+            zIndex: '-1',
+          }}>
+          <source
+            src={process.env.PUBLIC_URL + '/maquina-laser-18533.mp4'}
+            type='video/mp4'
+          />
+        </video>
+        <div className='overlay'></div>
+        <div className='title-background'>
+          <h1 className='title'>Skb Laser Tracer</h1>
+          <div className='underline'></div>
+          <FileUploader onFileChange={handleFileChange} />
+        </div>
+      </div>
       {file && <PDFRenderer file={file} />}
     </div>
   )
