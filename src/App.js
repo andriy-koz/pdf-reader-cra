d0a4e9c4433a98903a27a88c5dc05b2948b56716
Andriy-Kozlovsky <reactiivado.-@hotmail.com>
2023-05-21 22:43:21 -0300

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,35 @@
-import { useState, useEffect } from 'react'
-import LoadingBar from 'react-top-loading-bar'
-import { animateScroll as scroll } from 'react-scroll'
+import { useState, useEffect } from 'react';
+import LoadingBar from 'react-top-loading-bar';
 import {
   // eslint-disable-next-line no-unused-vars
   BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
-} from 'react-router-dom'
-import Home from './components/Home'
-import FileLoaded from './components/FileLoaded'
-import './styles.css'
+} from 'react-router-dom';
+import Home from './components/Home';
+import FileLoaded from './components/FileLoaded';
+import './styles.css';
 
 function App() {
-  const [file, setFile] = useState(null)
-  const [progress, setProgress] = useState(0)
-  const navigate = useNavigate()
+  const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(0);
+  const navigate = useNavigate();
 
   const handleFileChange = newFile => {
-    setProgress(30)
-    setFile(newFile)
-    navigate('/file-loaded', { state: { file: newFile } })
-  }
-
-  const scrollToPercentage = percentage => {
-    const pixelPosition =
-      document.documentElement.scrollHeight * (percentage / 100)
-    scroll.scrollTo(pixelPosition)
-  }
+    setProgress(30);
+    setFile(newFile);
+    navigate('/file-loaded', { state: { file: newFile } });
+  };
 
   useEffect(() => {
     if (file) {
-      setProgress(100)
+      setProgress(100);
       setTimeout(() => {
-        setProgress(0)
-        scrollToPercentage(100)
-      }, 1000)
+        setProgress(0);
+      }, 1000);
     }
-  }, [file])
+  }, [file]);
 
   return (
     <div className='app'>
@@ -51,7 +43,7 @@ function App() {
         <Route path='/file-loaded' element={<FileLoaded file={file} />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
